02877595819f5578cbc4306930bd4a12e367aaee
VALDELAINE CRISTINA RIBEIRO <valdelaine.ribeiro4@gmail.com>
2022-09-14 22:02:34 -0300

    Desafios
    Desafios desenvolvidos durante as aulas de Desenvolvimento Web II.

---
+++ login/cadastro.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Cadastro</h2>
+        <p>Favor inserir login e senha.</p>
+        <form action="arquivos.php" method="post">
+            <div class="form-group">
+                <label>Nome</label>
+                <input type="text" name="nome" class="form-control">
+                <span class="help-block"></span>
+            </div>   
+            <div class="form-group">
+                <label>CPF</label>
+                <input type="password" name="cpf" class="form-control">
+                <span class="help-block"></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Cadastrar">
+            </div>
+        </form>
+    </div>    
+</body>
+</html>
